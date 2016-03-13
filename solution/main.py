52880eb313dc8d8165b355769098d8eb758d09d9
Liang Shuailong <zju_shuailong@163.com>
2016-03-14 00:12:21 +0800

    update dataset and figures

--- solution/main.py
+++ solution/main.py
@@ -17,6 +17,7 @@ from sklearn import svm
 from time import time
 import numpy as np
 from random import shuffle
+import matplotlib.pyplot as plt
 
 def score(y_predict, y_true):
     '''
@@ -27,7 +28,7 @@ def score(y_predict, y_true):
     '''
 
     if len(y_predict) != len(y_true):
-        raise ValueError('Check the lengths of two arrays!')
+        raise ValueError('Check the lengths of two arrays: ' + str(len(y_predict)) + ' != ' + str(len(y_true)))
 
     accuracy = 0
     for i in range(len(y_predict)):
@@ -40,11 +41,11 @@ def find_optimal_C(trainX, trainY):
     N = len(trainX)
     M = len(trainX[0])
 
-    trainXPos = np.asarray([trainX[i, :] for i in range(len(trainX)) if trainY[i] == 1])
-    trainXNeg = np.asarray([trainX[i, :] for i in range(len(trainX)) if trainY[i] == -1])
+    trainXPos = trainX[trainY == 1]
+    trainXNeg = trainX[trainY == -1]
 
     if len(trainXPos) == 0 or len(trainXNeg) == 0:
-    # raise ValueError('Only one class in training set!')
+        raise ValueError('Only one class in training set!')
         pass
 
     ordersPos = range(len(trainXPos))
@@ -52,12 +53,12 @@ def find_optimal_C(trainX, trainY):
     ordersNeg = range(len(trainXNeg))
     shuffle(ordersNeg)
 
-    cvX = np.asarray([trainXPos[ordersPos[i], :] for i in range(len(trainXPos)/5)] + [trainXNeg[ordersNeg[i], :] for i in range(len(trainXNeg)/5)])
-    cvY = np.asarray([1] * (len(trainXPos)/5) + [-1] * (len(trainXPos)/5))
-
-    trainX = np.asarray([trainXPos[ordersPos[i], :] for i in range(len(trainXPos)/5, len(trainXPos))] + [trainXNeg[ordersNeg[i], :] for i in range(len(trainXNeg)/5, len(trainXNeg))])
+    cvX = np.concatenate([trainXPos[ordersPos[:len(trainXPos)/5]], trainXNeg[ordersNeg[:len(trainXNeg)/5]]])
+    cvY = np.asarray([1] * (len(trainXPos)/5) + [-1] * (len(trainXNeg)/5))
+    
+    trainX = np.concatenate([trainXPos[ordersPos[len(trainXPos)/5:len(trainXPos)]], trainXNeg[ordersNeg[len(trainXNeg)/5:len(trainXNeg)]]])
     trainY = np.asarray([1] * (len(trainXPos)*4/5) + [-1] * (len(trainXNeg)*4/5))
-
+    
     optimal_C = []
     optimal_cv_accuracy = 0
 
@@ -90,33 +91,42 @@ def find_optimal_C(trainX, trainY):
 def main():
     start_time = time()
 
-    for dataset in ['A', 'C']:
+    for dataset in ['A', 'B', 'C']:
 
         print 'Dataset:', dataset
 
         trainX, trainY = read_data(dataset, 'train')
         testX, testY = read_data(dataset, 'test')
 
-        # optimal_C = find_optimal_C(trainX, trainY)
-        C = 0.001
-        while C <= 1000:    
-            print 'C:', C
-            clf = mysvm.SVC(C=1.0, is_dual=False, kernel='linear')
-            # clf = svm.SVC(C=C, kernel='linear')
-            clf.fit(trainX, trainY)
-            train_Y = clf.predict(trainX)
-            train_score = score(train_Y, trainY)
-            test_Y = clf.predict(testX)
-            test_score = score(test_Y, testY)
+        # trainXPos = trainX[trainY == 1]
+        # trainXNeg = trainX[trainY == -1]
 
-            print 'Number of SVs:', clf.n_support_
-            # print 'Margin:', clf.margin
+        # plt.plot(trainXPos[:,0], trainXPos[:,1], 'ro')
+        # plt.plot(trainXNeg[:,0], trainXNeg[:,1], 'bo')
 
-            print '---------------------------------------'
-            print 'Training/test accuracy:', str(round(train_score*100, 2)) + '%', '/', str(round(test_score*100, 2)) + '%'
-            print '---------------------------------------'
-            print 
-            C *= 10
+        # plt.show()
+        # continue
+
+        optimal_C = find_optimal_C(trainX, trainY)
+        # C = 0.001
+        # while C <= 1000:    
+        #     print 'C:', C
+        clf = mysvm.SVC(C=optimal_C, is_dual=False)
+        # clf = svm.SVC(C=optimal_C)
+        clf.fit(trainX, trainY)
+        train_Y = clf.predict(trainX)
+        train_score = score(train_Y, trainY)
+        test_Y = clf.predict(testX)
+        test_score = score(test_Y, testY)
+
+        # print 'Number of SVs:', sum(clf.n_support_)
+        # print 'Margin:', clf.margin
+
+        print '---------------------------------------'
+        print 'Training/test accuracy:', str(round(train_score*100, 2)) + '%', '/', str(round(test_score*100, 2)) + '%'
+        print '---------------------------------------'
+        print 
+            # C *= 10
 
 
     print '----------' + str(round(time() - start_time, 2)) + ' seconds.---------------'
